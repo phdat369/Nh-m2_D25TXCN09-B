2e14335dc8374095dab7cd786253de04b368c238
phdat369 <phdat369@gmail.com>
2025-11-16 13:35:46 +0700

    Update BaiTapNhom2.cpp

--- BaiTapNhom2.cpp
+++ BaiTapNhom2.cpp
@@ -5,13 +5,32 @@
 #include <ctype.h>
 float array[100];
 int n=0;
-void nhap_diem(float array[]){
+int nhap_diem(float array[]){
+	int flag = 1 ; 
 	printf("\nNhap so luong diem : ");
 	scanf("%d",&n);
-	printf("\nNhap cac con diem cua sinh vien tu 0 -> 10 : ");
+	if ( n <= 0 || n > 100 ){
+		printf("So luong diem khong hop le ! ");
+		n = 0 ;
+		return 0; 
+	} 
+	printf("\nNhap cac con diem cua sinh vien : ");
+	flag = 1; 
 	for(int i=0;i<n;i++){
-		scanf("%f",&array[i]);
+	do {
+		printf("\nHay nhap diem thu %d : " , i + 1 ); 
+		scanf("%f",&array[i]);  
+		if ( array[i] < 0 || array[i] > 10   ){
+			printf("Diem khong hop le ! Nhap lai diem thu %d : " , i+1 ); 
+		} 
+     } while ( array[i] < 0 || array[i] > 10 ); 
 	}
+	if ( flag == 1 ){
+		printf("Tat ca diem deu hop le ! \n"); 
+	} else {
+		printf("Co diem khong hop le trong danh sach ! \n Vui long nhap lai diem \n"); 
+	} 
+	return flag ; 
 }
 void hien_thi_diem(float array[]){
 	if(n==0){
@@ -43,17 +62,32 @@ void diemCaoThap(float array[])
     printf("Diem cao nhat: %.1f\n", max);
     printf("Diem thap nhat: %.1f\n", min);
 }
-void themDiem(float array[],float k,int x){
+void themDiem(float array[]){
+	float k;
+	int x;
+	if(n>=100){
+		printf("Danh sach da day,khong the them\n");
+		return;
+	}
 	printf("\nNhap diem muon them : ");
 	scanf("%f",&k);
-	printf("\nNhap vi tri muon them diem : ");
-	scanf("%d",&x);
-	for(int i=n-1;i>x;i--){
-		array[i+1]=array[i];
-	}
-	array[x]=k;
-	n++;
+	if (k < 0 || k > 10) {
+    printf("Diem khong hop le!\n");
+    return;
 }
+	printf("\nNhap vi tri muon them diem : ");
+	scanf("%d",&x);	
+	if (x < 1 || x > n + 1) {
+        printf("Vi tri khong hop le!\n");
+        return;
+    }
+	for(int i=n;i>=x;i--){
+		array[i]=array[i-1];
+	}
+	array[x-1]=k;
+	n++;
+	printf("Da them diem thanh cong ");
+    }
 void xoaDiem(float array[]) {
     if (n == 0) {
         printf("Khong co diem de xoa!\n");
@@ -92,11 +126,11 @@ void loc_diem(float array[]) {
         printf("\nVui long chon chuc nang 1 ");
         return;
     }
-    printf("Cac diem lon hon 8.0\n");
+    printf("Cac diem lon hon 8.0 : ");
     int found = 0;
     for (int i = 0; i < n; i++) {
         if (array[i] > 8.0) {
-            printf("Cac diem lon hon 8 : %.1f", array[i]);
+            printf(" %.1f ", array[i]);
             found = 1;
         }
     }
@@ -131,7 +165,7 @@ int main(){
 				diemCaoThap(array);
 			break;
 			case 4:
-				themDiem(array,k,x);
+				themDiem(array);
 			break;
 			case 5:
 				xoaDiem(array);
@@ -146,7 +180,6 @@ int main(){
 				return 0;
 			default:
 				printf("Loi vui long chon lai :");
-				scanf("%d",&choose);
 		} 
 	}while(choose!=8);
 }
